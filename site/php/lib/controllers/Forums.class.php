869357db8dc50611d0bf50df3636c41d1a23ccdd
John Beech <github@mkv25.net>
2014-10-08 00:33:46 +0100

    Wired up a holding page for Forums content.

---
+++ site/php/lib/controllers/Forums.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class Forums
+{
+	public function __construct($request)
+	{
+		$view = new TemplateView();
+		$view->baseUrl($request->base);
+		$view->title('Forums');
+		$view->eyecatch('Forums', "A place to get involved, make suggestions, and get support.");
+		
+		$view->addSingleColumn('Forums are not yet open, come back soon!');
+		$view->addSingleColumn('If you need to get in touch urgently, email at <a href="mailto:games@mkv25.net" >games@mkv25.net</a> or send a tweet to <a href="https://twitter.com/Markavian">@Markavian</a>.');
+		
+		$view->render();
+	}
+}
