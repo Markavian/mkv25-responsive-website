f89559d2025f77dec957526f682fa668a9d436c8
John Beech <github@mkv25.net>
2014-10-08 00:36:57 +0100

    Wired up a holding page for Store content.

---
+++ site/php/lib/controllers/Store.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class Store
+{
+	public function __construct($request)
+	{
+		$view = new TemplateView();
+		$view->baseUrl($request->base);
+		$view->title('Store');
+		$view->eyecatch('Store', "Buy full versions of our games here!");
+		
+		$view->addSingleColumn('The store is not yet open, come back soon!');
+		$view->addSingleColumn('If you need to get in touch urgently, email at <a href="mailto:games@mkv25.net" >games@mkv25.net</a> or send a tweet to <a href="https://twitter.com/Markavian">@Markavian</a>.');
+		
+		$view->render();
+	}
+}
