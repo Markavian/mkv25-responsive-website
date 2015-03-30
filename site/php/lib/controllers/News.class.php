6f23dfc6992010876f0f739d8c3a06e9845c3677
John Beech <github@mkv25.net>
2015-03-30 17:45:44 +0100

    Moved methods out to TwitterFormatter class.

--- site/php/lib/controllers/News.class.php
+++ site/php/lib/controllers/News.class.php
@@ -22,7 +22,7 @@ class News
 		{
 			if(isset($tweet->text))
 			{
-				$tweetHtml = News::formatTweet($tweet);
+				$tweetHtml = TwitterFormatter::renderTweet($tweet);
 				$view->addSingleHTMLColumn($tweetHtml);
 			}
 		}
@@ -34,60 +34,5 @@ class News
 		}
 		
 		$view->render();
-	}
-
-	public static function formatTweet($tweet)
-	{
-		ob_start();
-
-		$content = $tweet->text;
-		$postTime = strtotime($tweet->created_at);
-		$postDate = date("Y/m/d h:i:s", $postTime);
-		$rawTweet = print_r($tweet, true);
-
-		$parsedown = new Parsedown();
-		$content = News::preserveHashTags($content);
-		$content = $parsedown->text($content);
-		$content = News::reverseHashTags($content);
-
-		$media = News::renderMediaForTweet($tweet);
-
-		echo <<<END
-			<heading>Twitter</heading>
-			<tweet>
-				<date>$postDate</date>
-				<p>$content</p>
-				$media
-			</tweet>
-END;
-
-		return ob_get_clean();
-
-	}
-
-	public static function renderMediaForTweet($tweet)
-	{
-		ob_start();
-
-		if(isset($tweet->extended_entities))
-		{
-			$mediaEntities = $tweet->extended_entities->media;
-			foreach($mediaEntities as $key=>$mediaItem)
-			{
-				echo <<<END
-					<media><img src="$mediaItem->media_url_https" /></media>
-END;
-			}
-		}
-
-		return ob_get_clean();
-	}
-
-	private static function preserveHashTags($content) {
-		return str_replace("#", "[HASH]", $content);
-	}
-
-	private static function reverseHashTags($content) {
-		return str_replace("[HASH]", "#", $content);
 	}
 }
