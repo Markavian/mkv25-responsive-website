a475337de4a5f707404b632e52b8d10bce331e6b
Markavian <github@mkv25.net>
2015-04-04 15:21:33 +0100

    Fixed content reference links based on data stored in file system.

--- site/php/lib/models/ArticleFileSystemReader.class.php
+++ site/php/lib/models/ArticleFileSystemReader.class.php
@@ -40,7 +40,33 @@ class ArticleFileSystemReader
 
 		if(is_numeric($primaryId) && is_array($refArray))
 		{
+			$articles = $this->getAllContent();
 			
+			$primaryRef = Article::$ID_INDEX[$primaryId]->urlname;
+			
+			if (is_array($articles))
+			{
+				foreach($articles as $key => $article)
+				{
+					// Include the article if it matches as a forward ref
+					foreach($refArray as $ref)
+					{
+						if ($article->urlname == $ref)
+						{
+							$contentItems[$article->urlname] = $article;
+						}
+					}
+					
+					// Include the article if it matches as a backwards ref
+					foreach($article->linkedArticles as $ref)
+					{
+						if ($ref == $primaryRef)
+						{
+							$contentItems[$article->urlname] = $article;
+						}
+					}
+				}
+			}
 		}
 
 		return $contentItems;
