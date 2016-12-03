e99d74141165b7d1651d0347990d5a37e11a5427
John Beech <github@mkv25.net>
2016-12-03 13:32:18 +0000

    Fix whitespace

--- site/php/lib/helpers/ProxyFallback.class.php
+++ site/php/lib/helpers/ProxyFallback.class.php
@@ -3,21 +3,21 @@
 class ProxyFallback
 {
 	var $fallbacks;
-	
+
 	public function __construct($fallbackArray)
 	{
 		$this->fallbacks = Array();
-	
+
 		if (is_array($fallbackArray))
 		{
 			$this->fallbacks = $fallbackArray;
 		}
 	}
-	
+
 	public function __call($name, $arguments)
     {
 		$result = false;
-		
+
 		foreach($this->fallbacks as $key => $proxy)
 		{
 			if (method_exists($proxy, $name))
@@ -25,13 +25,13 @@ class ProxyFallback
 				/* echo sprintf("<pre>Proxy %s : calling %s(%s)</pre>", get_class($proxy), $name, implode(", ", $arguments)); */
 				$result = call_user_func_array(array($proxy, $name), $arguments);
 			}
-			
+
 			if ($result)
 			{
 				return $result;
 			}
 		}
-		
+
 		return false;
     }
 }
