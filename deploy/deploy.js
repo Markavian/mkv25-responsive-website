32712b59172d213b64e0efa339821d0e2ab79e6c
John Beech <johnbeech.github@mkv25.net>
2019-01-14 23:19:57 +0000

    Fix the issues with the new deployment script

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,5 +1,4 @@
 const ftpDeploy = require('ftp-deploy')
-const deployer = new ftpDeploy()
 const fs = require('fs')
 const path = require('path')
 
@@ -11,8 +10,8 @@ const defaultConfig = {
   password: passwords['mkv25-stage'].password, // optional, prompted if none given
   host: 'ftp.mkv25.net',
   port: 21,
-  localRoot: path.join(__dirname, '../'),
-  remoteRoot: '/',
+  localRoot: path.join(__dirname, '../site'),
+  remoteRoot: '/site',
   // include: ['*', '**/*'],      // this would upload everything except dot files
   include: [], // ['*.php', 'dist/*'],
   exclude: ['.ftp*', '.git*', '.hta*', 'deploy', '*.fdproj', 'tasklist.md', 'readme.md', '.sublime'], // ['dist/**/*.map'], // e.g. exclude sourcemaps - ** exclude: [] if nothing to exclude **
@@ -20,54 +19,94 @@ const defaultConfig = {
   forcePasv: true // Passive mode is forced (EPSV command is not sent)
 }
 
-const variations = {
-  'stage-images': {
-    include: ['site/images/**/*']
-  },
-  'stage-code': {},
-  'stage-all': {
-    include: ['site/**/*']
-  },
-  'stage-php': {},
-  'stage-php-lib': {},
-  'stage-scripts': {},
-  'stage-stylesheets': {},
-  'stage-templates': {},
-  'stage-content': {},
-  'stage-articles': {},
-  'stage-projects': {},
-  'live-release': {}
+const modes = {
+  'stage-images': () => deploy({
+    include: ['images/**/*']
+  }),
+  'stage-code': () => runModes(['stage-php', 'stage-scripts', 'stage-stylesheets', 'stage-templates']),
+  'stage-all': () => runModes(['stage-code', 'stage-content', 'stage-articles', 'stage-projects', 'stage-images']),
+  'stage-php': () => deploy({
+    include: ['php/**/*'],
+    exclude: ['php/lib/external/**/*']
+  }),
+  'stage-php-lib': () => deploy({
+    include: ['images/**/*']
+  }),
+  'stage-scripts': () => deploy({
+    include: ['scripts/**/*']
+  }),
+  'stage-stylesheets': () => deploy({
+    include: ['stylesheets/**/*']
+  }),
+  'stage-templates': () => deploy({
+    include: ['templates/**/*']
+  }),
+  'stage-content': () => deploy({
+    include: ['content/**/*']
+  }),
+  'stage-articles': () => deploy({
+    include: ['articles/**/*']
+  }),
+  'stage-projects': () => deploy({
+    include: ['projects/**/*']
+  }),
+  'live-release': () => deploy({
+    include: ['**/*'],
+    user: passwords['mkv25-live'].username,
+    password: passwords['mkv25-live'].password
+  })
 }
 
-deployer.on('uploading', function(data) {
-  console.log(data)
-  console.log('[Deploy] Uploading:', data.filename, ',', data.transferredFileCount, 'of', data.totalFilesCount, 'complete')
-})
+function createDeployer() {
+  const deployer = new ftpDeploy()
+  deployer.on('uploading', function(data) {
+    console.log('[Deploy] Uploading :', `${data.transferredFileCount} / ${data.totalFilesCount} ::: ${data.filename}`)
+  })
 
-deployer.on('uploaded', function(data) {
-  console.log(data)
-  console.log('[Deploy] Uploaded:', data.filename, ',', data.transferredFileCount, 'of', data.totalFilesCount, 'complete')
-})
+  deployer.on('uploaded', function(data) {
+    console.log('[Deploy] Uploaded  :', `${data.transferredFileCount} / ${data.totalFilesCount} ::: ${data.filename}`)
+  })
 
-deployer.on('log', function(data) {
-  console.log('[Deploy]', data)
-})
+  deployer.on('log', function(data) {
+    console.log('[Deploy]', data)
+  })
 
-async function deploy(mode) {
-  const variation = variations[mode] || false
-  if (variation) {
-    const ftpConfig = Object.assign({}, defaultConfig, variation)
-    try {
-      console.log('[Deploy]', mode, ':', variation)
-      const result = await deployer.deploy(ftpConfig)
-      console.log('[Deploy] Finished:', result)
-    }
-    catch (ex) {
-      console.log('[Deploy] Error:', ex)
-    }
-  } else {
-    console.log(`[Deploy] No variation found for mode (${mode})`)
+  return deployer
+}
+
+async function deploy(variation) {
+  const deployer = createDeployer()
+  const ftpConfig = Object.assign({}, defaultConfig, variation)
+  try {
+    console.log('[Deploy]', mode, ':', variation)
+    const result = await deployer.deploy(ftpConfig)
+    console.log('[Deploy] Finished:', result)
+  }
+  catch (ex) {
+    console.log('[Deploy] Error:', ex)
   }
 }
 
-deploy(mode)
+async function runModes(modes) {
+  const results = []
+  while(modes.length > 0) {
+    const mode = modes.shift()
+    let result = await run(mode)
+    results.push(results)
+  }
+  return results
+}
+
+async function run(mode) {
+  console.log('[Deploy]', mode)
+  const fn = modes[mode]
+  if (fn) {
+    await fn()
+    console.log('[Deploy] Completed', mode)
+  } else {
+    console.log(`[Deploy] No mode found with the name: (${mode})`)
+    console.log('  Available modes:', Object.keys(modes).join(', '))
+  }
+}
+
+run(mode)
