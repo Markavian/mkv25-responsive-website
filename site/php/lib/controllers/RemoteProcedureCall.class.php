e52ff1df9c5a12202d871550ca9eba1b0459a2b5
John Beech <johnbeech.github@mkv25.net>
2019-01-24 15:46:36 +0000

    Reformat spacing in RPC file

--- site/php/lib/controllers/RemoteProcedureCall.class.php
+++ site/php/lib/controllers/RemoteProcedureCall.class.php
@@ -115,23 +115,23 @@ class RemoteProcedureCall
 				$post_params[] = $key.'='.urlencode($val);
 		    }
 		}
-	    $post_string = implode('&', $post_params);
+    $post_string = implode('&', $post_params);
 
-	    $parts=parse_url($url);
+    $parts=parse_url($url);
 
-	    $fp = fsockopen($parts['host'],
-	        isset($parts['port'])?$parts['port']:80,
-	        $errno, $errstr, 30);
+    $fp = fsockopen($parts['host'],
+        isset($parts['port'])?$parts['port']:80,
+        $errno, $errstr, 30);
 
-	    $out = "POST ".$parts['path']." HTTP/1.1\r\n";
-	    $out.= "Host: ".$parts['host']."\r\n";
-	    $out.= "Content-Type: application/x-www-form-urlencoded\r\n";
-	    $out.= "Content-Length: ".strlen($post_string)."\r\n";
-	    $out.= "Connection: Close\r\n\r\n";
-	    if (isset($post_string)) $out.= $post_string;
+    $out = "POST ".$parts['path']." HTTP/1.1\r\n";
+    $out.= "Host: ".$parts['host']."\r\n";
+    $out.= "Content-Type: application/x-www-form-urlencoded\r\n";
+    $out.= "Content-Length: ".strlen($post_string)."\r\n";
+    $out.= "Connection: Close\r\n\r\n";
+    if (isset($post_string)) $out.= $post_string;
 
-	    fwrite($fp, $out);
-	    fclose($fp);
+    fwrite($fp, $out);
+    fclose($fp);
 	}
 
 	private static function registerCallback($methodName, $argsArray)
